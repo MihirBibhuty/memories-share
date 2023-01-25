4e6f509a3730418feda257b772668dccc76d0a4d
Mihir Bibhuty <77892621+MihirBibhuty@users.noreply.github.com>
2023-01-25 23:02:09 +0530

    mongoose fixed

--- server/index.js
+++ server/index.js
@@ -17,9 +17,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-// const CONNECTION_URL = 'mongodb+srv://Mihir:Mihir123@cluster0.jwmk0ep.mongodb.net/?retryWrites=true&w=majority';
 const PORT = 5000;
 
+mongoose.set('strictQuery', false);
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
