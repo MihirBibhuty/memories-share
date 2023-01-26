c20f80dbdde19079c16e937cc339effd81cffa72
Mihir Bibhuty <77892621+MihirBibhuty@users.noreply.github.com>
2023-01-26 19:13:53 +0530

    npm i

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container } from '@material-ui/core';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
@@ -12,10 +12,11 @@ const App = () => {
         <BrowserRouter>
             <Container maxWidth="lg">
                 <Navbar />
-                <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/auth" exact component={Auth} />
-                </Switch>
+                <Home />
+                {/* <Routes>
+                    <Route path="/" exact component={<Home />} />
+                    <Route path="/auth" exact component={<Auth />} />
+                </Routes> */}
             </Container>
         </BrowserRouter>
     )
